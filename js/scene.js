f20a98378a01bf12ea79c0ea4231fa5c3533da63
kd41 <kyled073@gmail.com>
2021-10-11 21:53:44 +0100

    Moved script into scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,3 +1,20 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+// create scene and camera with fov 75 degrees and min/max render distance of 0.1/1000
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set(0,0,100);
+camera.lookAt(0,0,0);
 
-        
+// create the renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xffffff, 1);
+document.body.appendChild(renderer.domElement);
+
+// render the scene
+function animate() {
+	requestAnimationFrame( animate );
+
+	renderer.render( scene, camera );
+}
+
+animate();			  			
