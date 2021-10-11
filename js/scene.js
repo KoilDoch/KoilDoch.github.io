c5697fedb92a108f6c2905b1eeb15fdafb291887
kd41 <kd41@hw.ac.uk>
2021-10-11 21:24:08 +0100

    put all in html file for now

--- js/scene.js
+++ js/scene.js
@@ -1,34 +1,3 @@
-import * as THREE from 'https://cdn.skypack.dev/three@<version>';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
-// create scene and camera with fov 75 degrees and min/max render distance of 0.1/1000
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-// create the renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// create a basic shape
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function rotate(x,y) {
-    cube.rotation.x += x;
-    cube.rotation.y += y;
-}
-
-// render the scene
-function animate() {
-    requestAnimationFrame( animate );
-    
-    rotate(0.01,0.01);
-
-    renderer.render( scene, camera );
-}
-
-animate();           
+        
